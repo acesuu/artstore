5a6aa8646ec30c67756439131fd6564c998699ea
Abir <abir.hossain2107@gmail.com>
2023-02-11 15:38:19 +0600

    Undid the changes to akib's files
    I had changed some files in previous commit, now undoing the changes to find faults

--- server/app.js
+++ server/app.js
@@ -18,9 +18,9 @@ const authRoute = require("./src/auth/authRoutes");
 const productRoute = require("./src/product/routes/productRoute");
 const categoryRoute = require("./src/product/routes/categoryRoute");
 
-// app.use("/api/v1/auth", authRoute);
-// app.use("/api/v1/category", categoryRoute);
-// app.use("/api/v1/product", productRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
 
 mongoose.connect(
   DB,
