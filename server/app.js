0312f6f8d437a122de96e214e836fa26640258cf
Abir <abir.hossain2107@gmail.com>
2023-02-11 12:36:48 +0600

    Established mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+const DB = process.env.MONGO_URI;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello I am node.js application");
+});
+
+mongoose.connect(
+  DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection : " + err);
+    }
+  }
+);
+// App Port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App running on ${port} port`);
+});
