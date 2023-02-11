834e8c3c2d21d58d0850ed77e331732eb91cffcf
Abir <abir.hossain2107@gmail.com>
2023-02-11 16:01:19 +0600

    Added cartmodel.js

---
+++ server/src/cart/cartModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  items: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+      },
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
