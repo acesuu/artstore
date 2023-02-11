82c42fe11fcdcf638d4874cdda4058f10c1dc4d5
unknown <ahnafakib@iut-dhaka.edu>
2023-02-11 15:07:31 +0600

    completed product and category route

--- server/app.js
+++ server/app.js
@@ -15,8 +15,14 @@ app.get("/", (req, res) => {
 });
 
 const authRoute = require("./src/auth/authRoutes");
+const productRoute = require("./src/product/routes/productRoute")
+const categoryRoute = require("./src/product/routes/categoryRoute")
+
 
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
+
 
 mongoose.connect(
   DB,
